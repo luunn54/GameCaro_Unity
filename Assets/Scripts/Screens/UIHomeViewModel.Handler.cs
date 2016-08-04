a6af88670a17e54b152c13c69ea1c57af5a8a5b7
Nguyen Ngoc Luu <nguyenchinhquan@gmail.com>
2016-08-05 01:51:16 +0700

    Add Setting view + Help View

---
+++ Assets/Scripts/Screens/UIHomeViewModel.Handler.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UIGianty;
+using UIGianty.MVVM;
+
+public partial class UIHomeViewModel : UIManScreen {
+
+#region Fields
+
+	// Your fields here
+#endregion
+
+#region Built-in Events
+	public override void OnShow (params object[] args)
+	{
+		base.OnShow (args);
+	}
+
+	public override void OnShowComplete ()
+	{
+		base.OnShowComplete ();
+	}
+
+	public override void OnHide ()
+	{
+		base.OnHide ();
+	}
+
+	public override void OnHideComplete ()
+	{
+		base.OnHideComplete ();
+	}
+#endregion
+
+#region Custom implementation
+	public void OnClickHelp(){
+		UIMan.Instance.ShowScreen (UIContentType.SCREEN_HELP);
+	}
+
+	private void OnClickPlay(){
+	}
+
+	public void OnClickSetting(){
+		UIMan.Instance.ShowScreen (UIContentType.SCREEN_SETTING);
+	}
+	// Your custom code here
+#endregion
+
+#region Override animations
+	/* Uncommend this for override show/hide animation of Screen/Dialog use tweening code
+	public override IEnumerator AnimationShow ()
+	{
+		return base.AnimationShow ();
+	}
+
+	public override IEnumerator AnimationHide ()
+	{
+		return base.AnimationHide ();
+	}
+
+	public override IEnumerator AnimationIdle ()
+	{
+		return base.AnimationHide ();
+	}
+	*/
+#endregion
+}
---
+++ Assets/Scripts/Screens/UISettingViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+using UIGianty;
+using UIGianty.MVVM;
+
+// This code is generated automatically by UIMan ViewModelGerenrator, please do not modify!
+
+public partial class UISettingViewModel : UIManScreen {
+
+
+}
