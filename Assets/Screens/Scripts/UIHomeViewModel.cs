d52f3038a89b4ab0185072eaf94ef2338394abb7
Nguyen Ngoc Luu <nguyenchinhquan@gmail.com>
2016-08-09 00:08:57 +0700

    remove copy file

--- Assets/Screens/Scripts/UIHomeViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using UIGianty;
-using UIGianty.MVVM;
-
-// This code is generated automatically by UIMan ViewModelGerenrator, please do not modify!
-public partial class UIHomeViewModel : UIManScreen {
-	
-
-}
